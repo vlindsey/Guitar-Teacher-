8adaa06dbb09f2e9c4553cb060a61a05a3de23dc
VLindsey <student@valerie-lindsey.local>
2015-12-01 10:15:38 -0600

    Initial Commit

---
+++ guitarTeacher.finalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guitarTeacher.finalProject
+//
+//  Created by VLindsey on 12/1/15.
+//  Copyright © 2015 Valerie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
